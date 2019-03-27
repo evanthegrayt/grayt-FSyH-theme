9b605fb2b7c5b9d5f7b0b794ae3cf359efba9a56
Evan Gray <egray1985@gmail.com>
2019-03-26 20:57:06 -0500

    Update test comments and screenshot

--- test/test.zsh
+++ test/test.zsh
@@ -1,11 +1,13 @@
-# This is an example code that is diverse and allows to test a theme
+# This is some benign code I paste into a terminal to view the colorscheme.
+# A lot of this was taken from the example on:
+# https://github.com/zdharma/fast-syntax-highlighting
+
 print "An example quite long string $with variable in it"
 local param1="text $variable" param2='other $variable'
 math=$(( 10 + HISTSIZ + HISTSIZE + $SAVEHIST )) size=$(( 0 ))
 
 for (( ii = 1; ii <= size; ++ ii )); do
-    if [[ "${cmds[ii]} string" = "| string" ]]
-    then
+    if [[ "${cmds[ii]} string" = "| string" ]]; then
         sidx=${buffer[(in:ii:)\$\(?#[^\\\\]\)]} # find opening cmd-subst
         (( sidx <= len )) && {
             eidx=${buffer[(b:sidx:ii)[^\\\\]\)]} # find closing cmd-subst
@@ -13,7 +15,7 @@ for (( ii = 1; ii <= size; ++ ii )); do
     fi
 done
 
-cd ../ && ls && cd $OLDPWD
+pushd ../ && ls -l /usr/local/bin/* && popd
 
 cat <<<$PATH"xyz"
 
